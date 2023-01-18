4b8dd514a8dcaf47407e667722eb908d6b23f451
jempson <jempson@clamom.com.br>
2023-01-18 16:56:12 -0300

    add pagination

--- plateforme/js/teste.js
+++ plateforme/js/teste.js
@@ -1,9 +1,29 @@
 $(document).ready(function(){
 
     $('button').click(function(){
-        var ola =
-        alert('ok')
 
+        var de = $('#de').val();
+        var a = $('#a').val();
+        const paginate = function (array, index, size) {
+            // transform values
+            index = Math.abs(parseInt(index));
+            index = index > 0 ? index - 1 : index;
+            size = parseInt(size);
+            size = size < 1 ? 1 : size;
+    
+            // filter
+            return [...(array.filter((value, n) => {
+                return (n >= (index * size)) && (n < ((index+1) * size))
+            }))]
+        }
+        
+        var array = [
+            {id: "1"}, {id: "2"}, {id: "3"}, {id: "4"}, {id: "5"}, {id: "6"}, {id: "7"}, {id: "8"}, {id: "9"}, {id: "10"}
+        ];
+
+        var transform = paginate(array, de, a);
+       
+        console.log(transform);
 
     });
 
--- teste.php
+++ teste.php
@@ -8,7 +8,25 @@
 </head>
 <body>
 
-    
+    Dé <select name="" id="de">
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+        <option value="6">6</option>
+        <option value="7">7</option>
+    </select>
+    <span> - </span>
+    à <select name="" id="a">
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+        <option value="6">6</option>
+        <option value="7">7</option>
+    </select>
 
     <br><br>
     <button>Testar</button>

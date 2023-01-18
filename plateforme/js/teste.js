17d2a59e93101ffa52272dda165080433be0f0f1
jempson <jempson@clamom.com.br>
2023-01-18 18:01:17 -0300

    add pagination table

--- plateforme/js/teste.js
+++ plateforme/js/teste.js
@@ -17,13 +17,39 @@ $(document).ready(function(){
             }))]
         }
         
-        var array = [
-            {id: "1"}, {id: "2"}, {id: "3"}, {id: "4"}, {id: "5"}, {id: "6"}, {id: "7"}, {id: "8"}, {id: "9"}, {id: "10"}
-        ];
+        // var array = [
+        //     {id: "1"}, {id: "2"}, {id: "3"}, {id: "4"}, {id: "5"}, {id: "6"}, {id: "7"}, {id: "8"}, {id: "9"}, {id: "10"}
+        // ];
 
-        var transform = paginate(array, de, a);
-       
-        console.log(transform);
+        var items = [ 
+            { id: 1, first_name: 'Arjun', last_name: 'A' },
+            { id: 2, first_name: 'Kalyan', last_name: 'J' },
+            { id: 3, first_name: 'Raj', last_name: 'Kiran' },
+            { id: 4, first_name: 'Naveen', last_name: 'A' },
+            { id: 5, first_name: 'Pravinh', last_name: 'A' },
+            { id: 6, first_name: 'Srinivas', last_name: 'S' },
+            { id: 7, first_name: 'Mahipal', last_name: 'K' },
+            { id: 8, first_name: 'Sathish', last_name: 'Y' },
+            { id: 9, first_name: 'Aravind', last_name: 'A' },
+            { id: 10, first_name: 'Phani', last_name: 'C' } 
+        ]
+
+        var transform = paginate(items, de, a);
+        var linhas = [];
+
+        if (transform && transform.length) {
+            var linhas = [];
+            transform.forEach(el => {
+                var linha = "<tr>";
+                linha += "<td>" + el.id + "</td>";
+                linha += "<td>" + el.first_name + "</td>";
+                linha += "<td>" + el.last_name + "</td>";
+                linha += "</tr>";
+                linhas.push(linha);
+            });
+            $('.table tbody').empty();
+            $('.table tbody').append(linhas);
+        }
 
     });
 
--- teste.php
+++ teste.php
@@ -4,32 +4,56 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <title>Teste PHP / JS</title>
 </head>
 <body>
 
-    Dé <select name="" id="de">
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4">4</option>
-        <option value="5">5</option>
-        <option value="6">6</option>
-        <option value="7">7</option>
-    </select>
-    <span> - </span>
-    à <select name="" id="a">
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4">4</option>
-        <option value="5">5</option>
-        <option value="6">6</option>
-        <option value="7">7</option>
-    </select>
+    <style>
+        .table{
+            width: 30%;
+        }
 
-    <br><br>
-    <button>Testar</button>
+        .container{
+            margin-top: 50px;
+        }
+    </style>
+
+    <div class="container">
+        Dé <select name="" id="de">
+            <option value="0">0</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+        </select>
+        <span> - </span>
+        à <select name="" id="a">
+            <option value="3">3</option>
+            <option value="6">6</option>
+            <option value="9">9</option>
+        </select>
+    
+        <br><br>
+        <button>Testar</button>
+    
+        <br><br><br>
+    
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>NOM</th>
+                    <th>PRENOM</th>
+                </tr>
+            </thead>
+            <tbody></tbody>
+        </table>
+    </div>
+
 
     <script src="./plateforme/js/jquery.js"></script>
     <script src="./plateforme/js/teste.js"></script>
